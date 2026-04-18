2b3c5b646f8cda6322b3c287f644a9a25c719201
Pavlo Starobub <amorallex@gmail.com>
2026-04-18 14:41:03 +0300

    refactor

--- src/routes/profiles.py
+++ src/routes/profiles.py
@@ -14,7 +14,6 @@ from security.interfaces import JWTAuthManagerInterface
 router = APIRouter()
 
 
-
 @router.post("/users/{user_id}/profile/", status_code=status.HTTP_201_CREATED)
 async def create_profile(
     user_id: int,
@@ -24,44 +23,29 @@ async def create_profile(
     db: Annotated[AsyncSession, Depends(get_db)],
 ):
     if not token:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Authorization header is missing"
-        )
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authorization header is missing")
 
     try:
         jwt_manager.verify_access_token_or_raise(token)
     except Exception as exc:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=str(exc)
-        )
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(exc))
 
     stmt = select(UserModel).filter_by(id=user_id)
     result = await db.execute(stmt)
     user = result.scalars().first()
 
     if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="User not found."
-        )
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found.")
 
     if not user.is_active:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="User not found or not active."
-        )
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found or not active.")
 
     stmt = select(UserProfileModel).filter_by(user_id=user_id)
     result = await db.execute(stmt)
     exc_profile = result.scalars().first()
 
     if exc_profile:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="User already has a profile."
-        )
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User already has a profile.")
 
     profile = UserProfileModel(
         user_id=user_id,
